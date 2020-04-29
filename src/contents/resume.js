247f2ee42299618ac7198a917fa8bc6907d9db8d
David Gehtman <davidgehtman.91@gmail.com>
2020-04-29 16:14:58 -0400

    updated resume

--- src/contents/resume.js
+++ src/contents/resume.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-import updatedResume from '../images/updatedResume.pdf';
+
 
 class Resume extends Component {
     render(){
         return(
             <div className="condiv">
            
-    <iframe src="https://docs.google.com/gview?url=https://github.com/davidgeht/Portfolio-React-/raw/master/updatedResume.pdf&embedded=true" width="100%" height="100%" frameborder="0"></iframe>
+    <iframe src="https://docs.google.com/gview?url=https://github.com/davidgeht/Portfolio-React-/raw/master/Davidresume-v2.pdf&embedded=true" width="100%" height="100%" frameborder="0"></iframe>
             </div>
             
 
