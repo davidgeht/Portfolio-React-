a9980a7b98f80e0971e3c18c996f978e9322c2f2
David Gehtman <davidgehtman.91@gmail.com>
2020-05-13 14:22:42 -0400

    changed some stuff

--- src/contents/home.js
+++ src/contents/home.js
@@ -5,7 +5,7 @@ class Home extends Component {
 render() {
 return (
 <div className="condiv home">
-<ReactTypingEffect className="typingeffect" text={[`Hi I am David`,`I am a web developer`, `Currently I just received my certificate at the Univeristy of Toronto's Full Stack Coding Bootcamp`]} speed={100} eraseDelay={700}/>
+<ReactTypingEffect className="typingeffect" text={[`Hi I'm David`,`Welcome to my page !`, `I'm a Full Stack Web Developer with a certificate from the University Of Toronto `, `Looking forward to collaborating ! `]} speed={100} eraseDelay={700}/>
 
 </div>
 )
--- src/contents/resume.js
+++ src/contents/resume.js
@@ -6,7 +6,7 @@ class Resume extends Component {
         return(
             <div className="condiv">
            
-    <iframe src="https://docs.google.com/gview?url=https://github.com/davidgeht/Portfolio-React-/raw/master/Davidresume-v2.pdf&embedded=true" width="100%" height="100%" frameborder="0"></iframe>
+    <iframe src="https://docs.google.com/gview?url=https://github.com/davidgeht/Portfolio-React-/raw/master/DavidResume-final.pdf&embedded=true" width="100%" height="100%" frameborder="0"></iframe>
             </div>
             
 
